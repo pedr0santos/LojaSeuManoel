ab73b67669ca328ae85588a570a5b1c528f2f021
Pedro Santos <pedrosantos009@hotmail.com>
2024-09-15 18:21:11 -0300

    Rotas Config:

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './presentation/components/templates/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent, // Envolvendo todas as rotas com o layout principal
+    children: [
+    ],
+  },
+];
