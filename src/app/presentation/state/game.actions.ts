e6e8770b93c1bb372e8c007f6cbe05f47f7ef735
Pedro Santos <pedrosantos009@hotmail.com>
2024-09-14 22:49:36 -0300

    Feat: Criando arquivos de configuração e regra de negocio.

---
+++ src/app/presentation/state/game.actions.ts
@@ -0,0 +1,7 @@
+// game.actions.ts
+import { createAction, props } from '@ngrx/store';
+import { Game } from '../core/domain/entities/game.entity';
+
+export const addGame = createAction('[Game] Add Game', props<{ game: Game }>());
+export const loadGames = createAction('[Game] Load Games');
+export const loadGamesSuccess = createAction('[Game] Load Games Success', props<{ games: Game[] }>());
